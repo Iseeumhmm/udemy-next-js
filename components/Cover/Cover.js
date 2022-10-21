8bff883e64d59641b1b4be09143b300f2189ef22
Rick Heffren <rheffren@resulta.com>
2022-10-20 23:20:33 -0300

    Added dynamic slugs

---
+++ components/Cover/Cover.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export const Cover = (props) => {
+    console.log(props.background)
+    const loaderProp =({ src }) => {
+        return src;
+    }
+
+    return (
+    <div className="h-screen bg-slate-800 relative min-h-[400px] flex justify-center items-center">
+        <Image 
+            alt="Cover" 
+            src={props.background} 
+            layout="fill" 
+            objectFit="cover" 
+            className="mix-blend-soft-light"
+            loader={loaderProp}
+        />
+        {props.children}
+    </div>
+    )
+};
