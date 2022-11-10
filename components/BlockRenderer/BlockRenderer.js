a6d20f975ec87cd338427a3779734de8d4172090
Rick Heffren <rheffren@resulta.com>
2022-11-10 17:30:22 -0400

    Gutenberg bocks

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,27 +1,42 @@
+import { CallToActionButton } from "components/CallToActionButton";
+import { Column } from "components/Column";
+import { Columns } from "components/Columns";
 import { Cover } from "components/Cover";
 import { Heading } from "components/Heading";
 import { Paragraph } from "components/Paragraph";
+import Image from "next/image";
 import { theme } from "theme";
 
 export const BlockRenderer = ({blocks}) => {
+
+    const loaderProp =({ src }) => {
+        return src;
+    }
+
     return blocks.map(block => {
         switch(block.name) {
-            case 'core/paragraph': {
-                return (<Paragraph 
-                key={block.id} 
-                textAlign={block.attributes.align} 
-                content={block.attributes.content} 
-                textColor={theme[block.attributes.textColor] || block.attributes.style?.color?.text}
-                />)
-            }
-            case 'core/heading': {
+            case 'acf/ctabutton': {
                 return (
-                <Heading 
-                    key={block.id} 
-                    level={block.attributes.level} 
-                    content={block.attributes.content}
-                    textAlign={block.attributes.textAlign}
-                 />
+                    <CallToActionButton 
+                        key={block.id} 
+                        buttonLabel={block.attributes.data.label}
+                        destination={block.attributes.data.destination || "/"}
+                        align={block.attributes.data.align}
+                    />
+                )
+            }
+            case 'core/column': {
+                return (
+                    <Column key={block.id} >
+                        <BlockRenderer blocks={block.innerBlocks} width={block.attributes.width} />
+                    </Column>
+                )
+            }
+            case 'core/columns': {
+                return (
+                    <Columns key={block.id} isStackedOnMobile={block.attributes.isStackedOnMobile} >
+                        <BlockRenderer blocks={block.innerBlocks} />
+                    </Columns>
                 )
             }
             case 'core/cover': {
@@ -31,7 +46,44 @@ export const BlockRenderer = ({blocks}) => {
                     </Cover>
                 );
             }
+            case 'core/block':
+            case 'core/group': {
+                return <BlockRenderer id={block.id} blocks={block.innerBlocks} />
+            }
+            case 'core/heading': {
+                return (
+                    <Heading 
+                        key={block.id} 
+                        level={block.attributes.level} 
+                        content={block.attributes.content}
+                        textAlign={block.attributes.textAlign}
+                    />
+                )
+            }
+            case 'core/paragraph': {
+                return (
+                    <Paragraph 
+                        key={block.id} 
+                        textAlign={block.attributes.align} 
+                        content={block.attributes.content} 
+                        textColor={theme[block.attributes.textColor] || block.attributes.style?.color?.text}
+                    />
+                )
+            }
+            case 'core/image': {
+                return (
+                    <Image 
+                        key={block.id} 
+                        src={block.attributes.url} 
+                        height={block.attributes.originalHeight}
+                        width={block.attributes.originalWidth}
+                        alt={block.attributes.alt || ""}
+                        loader={loaderProp}
+                    />
+                );
+            }
             default: 
+            console.log("[UNKNOWN BLOCK]: ", block)
             return null;
         }
     });
